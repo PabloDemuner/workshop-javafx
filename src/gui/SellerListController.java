df84b6c7d130a35182c174b6536a3c7a2ef5258d
Pablo <pablodiegodemuner@gmail.com>
2021-02-21 15:02:38 -0400

    Adicionado Lista de vendedores ao TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -15,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -23,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -38,8 +44,17 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private TableColumn<Seller, Integer> tableColumnId;
 
 	@FXML
-	private TableColumn<Seller, String> tableColmunName;
-
+	private TableColumn<Seller, String> tableColumnName;
+	
+	@FXML
+	private TableColumn<Seller, String> tableColumnEmail;
+	
+	@FXML
+	private TableColumn<Seller, Date> tableColumnBirthDate;
+	
+	@FXML
+	private TableColumn<Seller, Double> tableColumnBaseSalary;
+	
 	@FXML
 	private TableColumn<Seller, Seller> tableColumnEdit;
 
@@ -79,7 +94,12 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private void initializeNodes() {
 
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
-		tableColmunName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
 		// Faz o tableView acompanhar o tamanho da Janela
 		Stage stage = (Stage) Main.getMainScene().getWindow();
@@ -109,7 +129,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 	 */
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
 
-		/*try {
+		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
 
@@ -131,7 +151,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}*/
+		}
 	}
 
 	@Override
@@ -182,7 +202,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 		});
 	}
 	//Metodo para remover e alertar sobre remoção de Departamento
-	protected void removeEntity(Seller obj) {
+	private void removeEntity(Seller obj) {
 		
 		Optional<ButtonType> results = Alerts.showConfirmation("Confirmação", "Deseja realmente deletar ?");
 		
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,28 +1,69 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Utils {
 
 	/*
-	 * Metodo para acessar um Stage de controle do evento
-	 * por exemplo ao clicar em um botão voce acessa o Stade daquele botão
+	 * Metodo para acessar um Stage de controle do evento por exemplo ao clicar em
+	 * um botão voce acessa o Stade daquele botão
 	 */
 	public static Stage currentStage(ActionEvent event) {
-		
-		return  (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
-	
-	//Metodo que converte o valor digitado para inteiro (tryParseToInt)
+
+	// Metodo que converte o valor digitado para inteiro (tryParseToInt)
 	public static Integer tryParseToInt(String str) {
 		try {
 			return Integer.parseInt(str);
-		}
-		catch (NumberFormatException exception) {
+		} catch (NumberFormatException exception) {
 			return null;
 		}
 	}
-	
+
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
+
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
 }
